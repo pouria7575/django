babc9e7e214ffb9b6f6cbbb40f18766ac36888d0
pouria7575 <p.pourrashidy@gmail.com>
2018-07-18 12:45:53 +0430

    make some changes for beauti

--- web/admin.py
+++ web/admin.py
@@ -4,6 +4,9 @@ from __future__ import unicode_literals
 from django.contrib import admin
 
 # Register your models here.
-from models import Expense
+from models import Expense,Income
 
 admin.site.register(Expense)
+
+admin.site.register(Income)
+
